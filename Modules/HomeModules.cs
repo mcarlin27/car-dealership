765023827cf72250682aba991232644e71ba2e9d
Marilyn Carlin and phat nguyen <marilyn+phat@>
2017-05-23 11:18:35 -0700

    begin refactoring Cars.cs

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using Cars.Objects;
+using System.Collections.Generic;
+
+namspace Cars
+{
+  public class HomeModule: NancyModule
+  {
+    public HomeModule()
+    {
+      
+    }
+  }
+}
